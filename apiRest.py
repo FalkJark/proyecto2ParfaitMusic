cd6f3f4daaae2b4ce0e97849218605782a9c1630
FalkJark <73451426+FalkJark@users.noreply.github.com>
2020-11-07 17:35:18 -0600

    Cambios para Heroku

--- apiRest.py
+++ apiRest.py
@@ -365,4 +365,4 @@ def login():
 
 # run app, define port and activate debug mode
 if __name__ == "__main__":
-    app.run(debug=True,port=5000)
+    app.run(threaded=True,host="0.0.0.0",port=5000,debug=True)
